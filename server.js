33ab7df6c212bb227993f7241beec41d38f079ac
harshada-1591 <harshadapatil1501@gmail.com>
2024-03-13 13:01:12 +0530

    add server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Transflower Store</h1>');
+  res.end('<h1>Hi Harshada is here</h1>');
 });
 server.listen(7000, () => {
   console.log(`Server running at 7000`);
