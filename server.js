15b7de701289786e335b2440816f2a5613464e98
harshada-1591 <harshadapatil1501@gmail.com>
2024-03-13 12:55:57 +0530

    code change

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+
+//import library
 const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Transflower Store</h1>');
 });
-server.listen(port, hostname, () => {
+server.listen(7000, () => {
   console.log(`Server running at 7000`);
 });
